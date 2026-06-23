7e344cfbddf8a8d37bb8c215a1af83a52317e529
muskanbharti <bhartimuskancse27@gmail.com>
2026-06-24 00:10:42 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+#list
+num=[10,20,30,40]
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[-1])
+#add mango 
+fruits = ["apple", "banana"]
+fruits.append("mango")
+print(fruits)
