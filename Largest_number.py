ac59ed34cf298697cd33e5333fc676a46fd05dc1
muskanbharti <bhartimuskancse27@gmail.com>
2026-06-25 21:00:08 +0530

    Add largest number in a list program

---
+++ Largest_number.py
@@ -0,0 +1,11 @@
+# largest number in a list
+
+numbers = [12, 45, 7, 89, 23]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest)
