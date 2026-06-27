b049749f9a802a31249924fd47a199ef8ef9a045
muskanbharti <bhartimuskancse27@gmail.com>
2026-06-27 16:47:53 +0530

    Add files via upload

---
+++ Loop_in list.py
@@ -0,0 +1,18 @@
+Marks=["Aman",50,98,70,68]
+for marks in Marks:
+    print(marks)
+
+## Print Each Number
+numbers = [10, 20, 30, 40, 50]
+for num in numbers:
+    print(num)
+
+## print squares
+numbers=[1,2,3,4,5,6,7,8,9,10]
+for num in numbers:
+    print(num**2)
+
+##print cubes
+numbers=[1,2,3,4,5,6,7,8,9,10]
+for num in numbers:
+    print(num**3)
